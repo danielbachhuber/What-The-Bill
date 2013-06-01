716beacb0832671f02da8730e3735244be0261de
Daniel Bachhuber <d@danielbachhuber.com>
2013-06-01 21:29:49 +0000

    Really pretty permalinks

--- functions.php
+++ functions.php
@@ -6,3 +6,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI )
 require_once dirname( __FILE__ ) . '/plugins/custom-metadata/custom_metadata.php'; 
 
 require_once dirname( __FILE__ ) . '/post-types/bill.php';
+
+add_filter( 'pre_option_permalink_structure', function(){
+	return '%postname%';
+});
